262442f859e1bdf3b631ec597c143b532d007131
9werayut <werayut.ku@gmail.com>
2019-08-04 22:41:27 +0700

    ระบบตรวจจับตา (Eye Detection)

--- Video.py
+++ Video.py
@@ -1,6 +1,7 @@
 import cv2
 
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+eyeCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye_tree_eyeglasses.xml")
 
 def draw_boundary(img, classifier, scaleFector, minNeighbors, color,text):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -9,11 +10,13 @@ def draw_boundary(img, classifier, scaleFector, minNeighbors, color,text):
     for(x,y,w,h) in features:
         cv2.rectangle(img, (x,y), (x+w,y+h), color,2)
         cv2.putText(img, text,(x,y-4),cv2.FONT_HERSHEY_SIMPLEX,0.8,color,2)
-    return img
+        coords=[x,y,w,h]
+    return img,coords
 
 
 def detect(img, faceCascade):
-    img = draw_boundary(img,faceCascade,1.1,10,(0,0,255),"Face")
+    img,coords = draw_boundary(img,faceCascade,1.1,10,(0,0,255),"Face")
+    img,coords = draw_boundary(img,eyeCascade,1.1,10,(255,0,0),"Eye")
     return img
 
 
