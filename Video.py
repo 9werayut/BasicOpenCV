0b41d1a182493741c5facc63826ae7ff22e2911e
9werayut <werayut.ku@gmail.com>
2019-08-04 22:27:28 +0700

    ระบบตรวจจับใบหน้า (Face Detection)

--- Video.py
+++ Video.py
@@ -1,9 +1,27 @@
 import cv2
+
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+def draw_boundary(img, classifier, scaleFector, minNeighbors, color,text):
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    features = classifier.detectMultiScale(gray,scaleFector,minNeighbors)
+    coords=[]
+    for(x,y,w,h) in features:
+        cv2.rectangle(img, (x,y), (x+w,y+h), color,2)
+        cv2.putText(img, text,(x,y-4),cv2.FONT_HERSHEY_SIMPLEX,0.8,color,2)
+    return img
+
+
+def detect(img, faceCascade):
+    img = draw_boundary(img,faceCascade,1.1,10,(0,0,255),"Face")
+    return img
+
+
 cap = cv2.VideoCapture("Video.mp4")
 while (True):
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame', gray)
+    frame=detect(frame, faceCascade)
+    cv2.imshow('frame', frame)
     if(cv2.waitKey(1) & 0xFF == ord('q')):
         break
 
