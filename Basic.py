1c054159fac790ea9c7e5c46a3459d2b0e77c626
9werayut <werayut.ku@gmail.com>
2019-08-04 21:44:12 +0700

    Drawing Function

--- Basic.py
+++ Basic.py
@@ -1,5 +1,10 @@
 import cv2
 img = cv2.imread("elephant.jpg", cv2.IMREAD_COLOR)
+#img = cv2.line(img,(0,0),(255,255),(0,0,255),10)
+#img = cv2.arrowedLine(img,(0,0),(400,400),(255,0,0),10)
+#img = cv2.rectangle(img,(384,0),(510,128),(0,0,255),10)
+#img = cv2.circle(img, (447,63), 63,(0,255,0),-1)
+img = cv2.putText(img, "OpenCV", (10,100),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
 cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
