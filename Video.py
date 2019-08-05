c08b0c7fa57c4f8004c3f32b6409bab6b7c01f6b
9werayut <werayut.ku@gmail.com>
2019-08-05 22:34:15 +0700

    สร้าง dataset

--- Video.py
+++ Video.py
@@ -1,7 +1,10 @@
 import cv2
 
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-eyeCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye_tree_eyeglasses.xml")
+
+def create_dataset(img, id, img_id):
+    cv2.imwrite("data/pic."+str(id)+"."+str(img_id)+".jpg", img)
+
 
 def draw_boundary(img, classifier, scaleFector, minNeighbors, color,text):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -14,17 +17,23 @@ def draw_boundary(img, classifier, scaleFector, minNeighbors, color,text):
     return img,coords
 
 
-def detect(img, faceCascade):
+def detect(img, faceCascade,img_id):
     img,coords = draw_boundary(img,faceCascade,1.1,10,(0,0,255),"Face")
-    img,coords = draw_boundary(img,eyeCascade,1.1,10,(255,0,0),"Eye")
+    
+    if len(coords)==4 :
+        id=1
+        result = img[coords[1]:coords[1]+coords[3],coords[0]:coords[0]+coords[2]]
+        create_dataset(result,id,img_id)
+
     return img
 
-
+img_id=0
 cap = cv2.VideoCapture("Video.mp4")
 while (True):
     ret, frame = cap.read()
-    frame=detect(frame, faceCascade)
+    frame=detect(frame, faceCascade,img_id)
     cv2.imshow('frame', frame)
+    img_id+=1
     if(cv2.waitKey(1) & 0xFF == ord('q')):
         break
 
